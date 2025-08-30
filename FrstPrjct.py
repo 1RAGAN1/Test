887a43f45267659ae7c158d1f320086d6dc3ba1d
1RAGAN1 <sounakganguly001@gmail.com>
2025-08-30 14:43:37 +0530

    Update all files with latest changes

--- FrstPrjct.py
+++ FrstPrjct.py
@@ -1,7 +1,22 @@
 import pandas as pd
-# save filepath to variable for easier access
-melbourne_file_path = 'data/melb_data.csv'  
+
+# Melbourne data
+melbourne_file_path = 'data/Melb/melb_data.csv'  
 # read the data and store data in DataFrame titled melbourne_data
 melbourne_data = pd.read_csv(melbourne_file_path) 
 # print a summary of the data in Melbourne data
-print(melbourne_data.describe())
+#print(melbourne_data.describe())
+#print(melbourne_data.head())
+print(melbourne_data.columns)
+
+
+# Iowa data
+iowa_file_path = 'data/Iowa/train.csv'
+# Fill in the line below to read the file into a variable home_data
+home_data = pd.read_csv(iowa_file_path)
+# Print summary statistics in next line
+#print(home_data.describe())
+# What is the average lot size (rounded to nearest integer)?
+avg_lot_size = home_data['LotArea'].mean().round()
+# As of today, how old is the newest home (current year - the date in which it was built)
+newest_home_age = 2025 - home_data['YearBuilt'].max()
